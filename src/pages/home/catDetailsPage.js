c6563079e4c241a9cf91876d3d4fff5d69e46a8a
Mithun Prem <mithunprem19@gmail.com>
2020-03-25 10:21:21 +1100

    Add a landing page that will display the header and the cat details (once the data is fetched) and use it in App.js

---
+++ src/pages/home/catDetailsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PageHeader from '../../components/header/headerView';
+
+const CatDetailsPage = () => {
+
+  return (
+    <>
+      <PageHeader />
+    </>
+  )
+}
+
+export default CatDetailsPage;
