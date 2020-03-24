88c88086f3c8408d11dfd91d23da7a55778997f4
Mithun Prem <mithunprem19@gmail.com>
2020-03-25 10:20:40 +1100

    Add a header component.

---
+++ src/components/header/headerView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/header.scss';
+
+const Header = () => {
+  return (
+    <header className='cat-details-header'>
+      <h2 className='header-text'>Cat Details</h2>
+    </header>
+  );
+}
+
+export default Header;
